c6c35c70f67a0262b925c09d39a9866f4208bd91
Paul041998 <amsterdampawloamsterdam@gmail.comgit config --global user.email amsterdampawloamsterdam@gmail.com>
2025-03-12 21:03:00 +0200

    header styles added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
-import '../scss/style.scss'
+import "../scss/style.scss";
+
+(function () {
+  const isOpen = document.querySelector(".header__burger");
+  const isClose = document.querySelector(".header__menu-close");
+  const navigation = document.querySelector(".header__menu");
+
+  if (isOpen && navigation) {
+    isOpen.addEventListener("click", () => {
+      navigation.classList.toggle("active");
+    });
+  }
+
+  if (isClose && navigation) {
+    isClose.addEventListener("click", () => {
+      navigation.classList.remove("active");
+    });
+  }
+
+  document.addEventListener("click", (event) => {
+    if (
+      navigation &&
+      !navigation.contains(event.target) &&
+      isOpen &&
+      !isOpen.contains(event.target)
+    ) {
+      navigation.classList.remove("active");
+    }
+  });
+})();
